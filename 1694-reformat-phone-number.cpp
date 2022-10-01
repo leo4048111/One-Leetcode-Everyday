57275de36107ac3c19f862c3a4b46f2f651000bd
Potato5K <74029782+leo4048111@users.noreply.github.com>
2022-10-01 13:36:41 +0800

    20221001
    20221001

---
+++ 1694-reformat-phone-number.cpp
@@ -0,0 +1,29 @@
+//https://leetcode.cn/problems/reformat-phone-number/
+
+class Solution {
+public:
+    string reformatNumber(string number) {
+        string s;
+        for(char& c: number)
+        {
+            if(c != '-' && c != ' ') s += c;
+        }
+        
+        string s2;
+        int mod = (s.size() % 3 == 1) ? 4 : (s.size() % 3);
+        for(int i = 0; i < (s.size() - mod) /3; i++)
+        {
+            s2 += s.substr(i * 3, 3);
+            s2 += '-';
+        }
+
+        for(int j = s.size() - mod; j < s.size(); j += 2)
+        {
+            s2 += s[j];
+            s2 += s[j + 1];
+            s2 += '-';
+        }
+
+        return s2.substr(0, s2.size() - 1);
+    }
+};
