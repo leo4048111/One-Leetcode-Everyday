ce6954cd21734f077859fd5d799bae25afaabcda
Potato5K <74029782+leo4048111@users.noreply.github.com>
2022-10-16 11:05:33 +0800

    20221016
    20221016

--- 886-possible-bipartition.cpp
+++ 886-possible-bipartition.cpp
@@ -31,3 +31,43 @@ public:
         return true;
     }
 };
+
+// solution with union-find disjoint sets
+class Solution {
+public:
+    bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
+        vector<int> p(n);
+        iota(p.begin(), p.end(), 0);
+
+        vector<vector<int>> graph(n);
+
+        for(auto& pair : dislikes)
+        {
+            graph[pair[0] - 1].push_back(pair[1] - 1);
+            graph[pair[1] - 1].push_back(pair[0] - 1);
+        }
+
+        function<int(int)> findAncestor = [&](int idx) -> int {
+            int father = p[idx];
+            if(father == p[idx]) return father;
+            else return findAncestor(father);
+        };
+
+        function<void(int, int)> Union = [&](int idx1, int idx2) -> void{
+            int a1 = findAncestor(idx1);
+            int a2 = findAncestor(idx2);
+            p[a1] = a2;
+        };
+
+        for(int i = 0; i < n; i++)
+        {
+            for(auto& dislikePerson : graph[i])
+            {
+                if(findAncestor(i) == findAncestor(dislikePerson)) return false;
+                Union(dislikePerson, graph[i][0]);
+            }
+        }
+
+        return true;
+    }
+};
