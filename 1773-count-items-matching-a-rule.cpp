38a7c4e1e7de34e460e1f85127cfb7bd03f3435d
Potato5K <74029782+leo4048111@users.noreply.github.com>
2022-10-29 16:59:53 +0800

    20221029
    20221029

---
+++ 1773-count-items-matching-a-rule.cpp
@@ -0,0 +1,15 @@
+// https://leetcode.cn/problems/count-items-matching-a-rule/
+
+class Solution {
+public:
+    int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
+        int ans = 0;
+        for(auto& item : items)
+        {
+            if(ruleKey == "type" && ruleValue == item[0] || ruleKey == "color" && ruleValue == item[1] || ruleKey == "name" && ruleValue == item[2])
+            ans++;
+        }   
+
+        return ans;
+    }
+};
