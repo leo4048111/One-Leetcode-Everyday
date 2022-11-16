9629dc97131e7948051c457dc1b3d3636980dcda
Potato5K <74029782+leo4048111@users.noreply.github.com>
2022-11-16 21:26:31 +0800

    20221116
    20221116

---
+++ 775-global-and-local-inversions.cpp
@@ -0,0 +1,12 @@
+// https://leetcode.cn/problems/global-and-local-inversions/
+
+class Solution {
+public:
+    bool isIdealPermutation(vector<int>& nums) {
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            if (abs(nums[i] - i) > 1) return false;
+        }
+        return true;
+    }
+};
