ddccaeb5fd749a0de0906ef163c0434a192a892c
LordAlmightyIsMySheperd <liqitong@163.com>
2023-07-24 22:14:12 +0800

    20230724
    20230724

---
+++ 771-jewels-and-stones.cpp
@@ -0,0 +1,13 @@
+// https://leetcode.cn/problems/jewels-and-stones/
+
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int ans = 0;
+        for(auto& c : stones) {
+            if(jewels.find(c) != string::npos) ans++;
+        }
+
+        return ans;
+    }
+};
