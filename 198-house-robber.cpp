54d7a787fb382ddb72dd252f0d8616aea030a45d
Z3r0M3m0ry <liqitong@163.com>
2023-09-16 11:12:39 +0800

    20230916

---
+++ 198-house-robber.cpp
@@ -0,0 +1,20 @@
+// https://leetcode.cn/problems/house-robber/description/?envType=daily-question&envId=2023-09-16
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, 0);
+
+        if(nums.size() == 1) return nums[0];
+
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+
+        for(int i = 2; i < nums.size(); i++) {
+            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+
+        return dp[nums.size() - 1];
+    }
+};
